65f8816cb316a95b26693f2d9f87d23e9f8e01f8
Maik Giraldo <michaelgiraldo40@gmail.com>
2020-09-07 16:19:02 -0500

    se añadio ruta de registro

---
+++ Agenda-me.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'agendame'
+
+# Ruta de registro de usuario
+@app.route('/registro')
+def registro():
+    
+
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
