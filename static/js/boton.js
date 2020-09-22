fa3af0f07a3e1de6dc980664ca9d5f27418926a1
Julian Sabogal <juliansabogal116@gmail.com>
2020-09-22 18:20:21 -0500

    modificando diseño para mejor comodidad

--- static/js/boton.js
+++ static/js/boton.js
@@ -34,3 +34,23 @@ function cambio2(){
     }
     
     btn2.addEventListener('click',cambio2,true)
+
+function muestra_oculta(id){
+    if (document.getElementById){ //se obtiene el id
+    var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+    }
+    }
+    window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+    muestra_oculta('div_flash');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+    }
+
+
+function ConfirmDelete()
+{
+  var x = confirm("¿Estas seguro de eliminar?");
+  if (x)
+      return true;
+  else
+    return false;
+}
